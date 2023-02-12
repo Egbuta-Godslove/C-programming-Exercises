38acfe85ecf7f587b2de758931e2999aebbc561f
Egbuta-Godslove <godsloveegbuta6@gmail.com>
2023-02-12 08:27:34 -0800

    nested-for-loop.c

---
+++ nested-for-loop.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+	  
+	  int i=2;
+	    int j=2;
+	      
+	      for(i=0; i+j; i++){
+		        for(j=0; j+i; j++){
+				      printf("\nthe value of i & j is %d", i);
+				            printf("\nthe value of j & i is %d", j);
+					      }
+			  }
+	          return 0;
+}
