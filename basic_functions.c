f3482129cced49843c5b57529d471013f22f3475
Egbuta-Godslove <godsloveegbuta6@gmail.com>
2023-02-17 07:10:51 -0800

    basic_functions.c

---
+++ basic_functions.c
@@ -0,0 +1,19 @@
+/* C CODE to implement function
+*/
+
+#include<stdio.h>
+
+void hello();
+
+void main(){
+hello();
+printf("\nI am calling the Function Hello");
+printf("\nYou can call multiple functions!!!");
+
+return (0);
+}
+void hello()
+
+{
+printf("Hello WOrld i am Function");
+}
