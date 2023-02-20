2d23f6a7cbe9189da835af96a024e02b3347aae9
Egbuta-Godslove <godsloveegbuta6@gmail.com>
2023-02-20 06:47:54 -0800

    learning

---
+++ wide-pointer.c
@@ -0,0 +1,13 @@
+//when we delcare a pointer in C & do not intialize it then such pointer is called as "Wild pointer".
+
+
+#include <stdio.h>
+
+viod main() {
+
+int *pointer
+	printf("%d", *pointer);
+return 0;
+}
+
+//In the above example *pointer is wild pointer because it do not points to anything inside the computer's memory!
