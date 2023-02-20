2e15bc3ecc0f7d05983104396c54d305698c162b
Egbuta-Godslove <godsloveegbuta6@gmail.com>
2023-02-20 07:11:41 -0800

    learning

---
+++ Null-pointer.c
@@ -0,0 +1,11 @@
+// C code to demostrate NULL pointer.
+
+#include <stdio.h>
+
+void main () {
+
+int *ptr = NULL; /*meaning of NULL is defined in stdio.h */
+printf("The value of ptr is %d", ptr);
+
+return 0;
+}
