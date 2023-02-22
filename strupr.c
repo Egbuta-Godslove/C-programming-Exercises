a9867a07676fd7c6dd3fcdb9b2cc19abfe36c72f
Egbuta-Godslove <godsloveegbuta6@gmail.com>
2023-02-22 02:31:03 -0800

    string to uppercase alphabets

---
+++ strupr.c
@@ -0,0 +1,12 @@
+//C Code to implement strupr Function
+#include <stdio.h>
+#include <string.h>
+
+void main() {
+
+char name[40]="c programming is fun";
+strupr(name);
+printf("%s", name);
+
+return 0;
+}
