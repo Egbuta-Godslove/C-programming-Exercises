40a5246ec8c3cc0f12d9b17960050ff01c5eeabd
Egbuta-Godslove <godsloveegbuta6@gmail.com>
2023-02-22 06:23:11 -0800

    string at the end of other

---
+++ strcat.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string h>
+
+void main (){
+
+char name [15]="Hello";
+char nane [15]="World";
+
+printf("name1 =%s \tname2 =%s",name1,name2);
+strcat(name1,name2);
+
+print("\nAfter using the string cate function ");
+printf("\n string at name1 is %s",name1);
+return(0);
+}
+
