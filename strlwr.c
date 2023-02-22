157dbf22c994888e2d25486aee30a99d7595da45
Egbuta-Godslove <godsloveegbuta6@gmail.com>
2023-02-22 02:38:40 -0800

    string to lower case

---
+++ strlwr.c
@@ -0,0 +1,12 @@
+//C Code to implement strlwr Function
+#include <stdio.h>
+#include <string.h>
+
+void main() {
+
+char name[40]="LET'S MAKE THE WORLD A BEAUTIFUL PLACE";
+strlwr(name);
+printf("%s", name);
+
+return 0;
+}
